f68ef499a30fa4dbbfc65a406163003e98dc5a96
windows 10 <dimpulsunayana205@gmail.com>
2023-01-03 21:03:59 +0530

    example shell

--- 01_example.sh
+++ 01_example.sh
@@ -11,5 +11,11 @@ myname=dimpul
 echo name = $myname
 echo "----dimmi---"
 
+# Color COde Syntax : echo -e "\e[COLmMESSAGE\e[0m"
+
+echo -e "\e[31myou are great dimpul\e[0m"
+
+
+
 # grep root user from user file
 #grep root /etc/passwd
